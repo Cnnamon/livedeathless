8d09c3bcbe7160605b79b00e9773eb19b46f51f6
Cnnamon <taututis@yahoo.com>
2016-01-21 23:01:31 +0200

    Live deathless: Initial commit

---
+++ Source/livedeathless/livedeathlessGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "livedeathlessGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIVEDEATHLESS_API AlivedeathlessGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
